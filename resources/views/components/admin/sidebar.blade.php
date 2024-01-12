23ad4995918a4ea22be80e948f91fec9fdb68218
Zafeer <zafeer.ad999@gmail.com>
2024-01-12 19:35:53 +0530

    template setup

---
+++ resources/views/components/admin/sidebar.blade.php
@@ -0,0 +1,67 @@
+<div class="app-menu navbar-menu">
+    <!-- LOGO -->
+    <div class="navbar-brand-box">
+        <!-- Dark Logo-->
+        <a href="index.html" class="logo logo-dark">
+            <span class="logo-sm">
+                <img src="{{ asset('admin/images/logo-sm.png') }}" alt="" height="22" />
+            </span>
+            <span class="logo-lg">
+                <img src="{{ asset('admin/images/logo-dark.png') }}" alt="" height="17" />
+            </span>
+        </a>
+        <!-- Light Logo-->
+        <a href="index.html" class="logo logo-light">
+            <span class="logo-sm">
+                <img src="{{ asset('admin/images/logo-sm.png') }}" alt="" height="22" />
+            </span>
+            <span class="logo-lg">
+                <img src="{{ asset('admin/images/logo-light.png') }}" alt="" height="17" />
+            </span>
+        </a>
+        <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
+            <i class="ri-record-circle-line"></i>
+        </button>
+    </div>
+
+    <div id="scrollbar">
+        <div class="container-fluid">
+            <div id="two-column-menu"></div>
+            <ul class="navbar-nav" id="navbar-nav">
+                <li class="menu-title">
+                    <span data-key="t-menu">Menu</span>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link menu-link" href="{{ route('dashboard') }}" >
+                        <i class="ri-dashboard-2-line"></i>
+                        <span data-key="t-dashboards">Dashboard</span>
+                    </a>
+                </li>
+
+
+                <li class="nav-item">
+                    <a class="nav-link menu-link" href="#sidebarLayouts" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarLayouts">
+                        <i class="ri-layout-3-line"></i>
+                        <span data-key="t-layouts">Masters</span>
+                        {{-- <span class="badge badge-pill bg-danger" data-key="t-hot">Hot</span> --}}
+                    </a>
+                    <div class="collapse menu-dropdown" id="sidebarLayouts">
+                        <ul class="nav nav-sm flex-column">
+                            <li class="nav-item">
+                                <a href="{{ route('wards.index') }}" class="nav-link" data-key="t-horizontal">Wards</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+
+    <div class="sidebar-background"></div>
+</div>
+
+
+<div class="vertical-overlay"></div>
---
+++ routes/web.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect()->route('login');
+})->name('/');
+
+
+
+
+// Guest Users
+Route::middleware(['guest','PreventBackHistory'])->group(function()
+{
+    Route::get('login', [App\Http\Controllers\Admin\AuthController::class, 'showLogin'] )->name('login');
+    Route::post('login', [App\Http\Controllers\Admin\AuthController::class, 'login'])->name('signin');
+    Route::get('register', [App\Http\Controllers\Admin\AuthController::class, 'showRegister'] )->name('register');
+    Route::post('register', [App\Http\Controllers\Admin\AuthController::class, 'register'])->name('signup');
+
+});
+
+
+
+
+// Authenticated users
+Route::middleware(['auth','PreventBackHistory'])->group(function()
+{
+
+    // Auth Routes
+    Route::get('home', fn () => redirect()->route('dashboard'))->name('home');
+    Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
+    Route::post('logout', [App\Http\Controllers\Admin\AuthController::class, 'Logout'])->name('logout');
+    Route::get('change-theme-mode', [App\Http\Controllers\Admin\DashboardController::class, 'changeThemeMode'])->name('change-theme-mode');
+    Route::get('show-change-password', [App\Http\Controllers\Admin\AuthController::class, 'showChangePassword'] )->name('show-change-password');
+    Route::post('change-password', [App\Http\Controllers\Admin\AuthController::class, 'changePassword'] )->name('change-password');
+
+
+
+    Route::resource('wards', App\Http\Controllers\Admin\Masters\WardController::class );
+
+});
+
+
+
+
+Route::get('/php', function(Request $request){
+    // if( !auth()->check() )
+    //     return 'Unauthorized request';
+
+    Artisan::call($request->artisan);
+    return dd(Artisan::output());
+});
